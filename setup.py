fdde4fa844849b599c4bed077f38eacea9745e6a
BeLeap <changseo_jang@korea.ac.kr>
2021-05-20 02:51:17 +0900

    fix name

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from crontab import CronTab
 
 cron = CronTab(user='root')
 job = cron.new('python3 ./main.py')
-job.dow.on('TUE', 'WED', 'THR', 'FRI', 'SAT')
+job.dow.on('TUE', 'WED', 'THU', 'FRI', 'SAT')
 job.hour.on(8)
 cron.write()
