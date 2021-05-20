9aa6cdc5770bb6c30805d23d7ecac3b834b2aa9b
BeLeap <changseo_jang@korea.ac.kr>
2021-05-20 14:58:43 +0900

    fix docker

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt update
 RUN apt install cron -y
 RUN cp -p /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 
+WORKDIR /thecamp-finance-bot
 COPY . .
 RUN pip3 install -r requirements.txt
 RUN python3 ./setup.py
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from crontab import CronTab
 
 cron = CronTab(user='root')
-job = cron.new('python3 ./main.py')
+job = cron.new('python3 ./src/main.py')
 job.dow.on('TUE', 'WED', 'THU', 'FRI', 'SAT')
 job.hour.on(8)
 cron.write()
